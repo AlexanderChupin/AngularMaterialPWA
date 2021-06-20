649412f6d95c3990e0e0a5effbc85ac5c897e086
AChupin <alexander.o.chupin@gmail.com>
2021-06-20 11:59:32 +0300

    2021-06-20 first working reproduction of the project. Removed unnecessary Google SSO fields.

--- partTwo/src/app/auth/sign-in/sign-in.component.ts
+++ partTwo/src/app/auth/sign-in/sign-in.component.ts
@@ -13,20 +13,20 @@ import { LoaderService } from 'src/app/loader/loader.service';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
-  
+
   signinForm: FormGroup = new FormGroup({
     email: new FormControl('',[ Validators.email, Validators.required ]),
     password: new FormControl('', [ Validators.required, Validators.min(6) ])
   });
-  
+
   hide = true;
 
   get emailInput() { return this.signinForm.get('email'); }
   get passwordInput() { return this.signinForm.get('password'); }
 
-  constructor( 
-    public auth: AuthService, 
-    private _notification: NotificationService, 
+  constructor(
+    public auth: AuthService,
+    private _notification: NotificationService,
     private _router: Router,
     private _loader: LoaderService ) { }
 
@@ -73,8 +73,8 @@ export class SignInComponent {
     console.log('fb Result:', socialResult);
   }
 
-  async signInWithGoogle() {
+/*  async signInWithGoogle() {
     const socialResult = await this.auth.socialSignIn(AuthService.GOOGLE);
     console.log('google Result:', socialResult);
-  }
+  }*/
 }
