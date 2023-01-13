0a4256830eb5a129e82f8b74a55daf8f7a14437f
AChupin <alexander.o.chupin@gmail.com>
2023-01-13 23:39:32 +0300

    ALC. Augmented unit testing of service by creating jasmine.createSpyObj

--- partTwo/src/app/services/alcwebsocket.service.spec.ts
+++ partTwo/src/app/services/alcwebsocket.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AlcwebsocketService } from './alcwebsocket.service';
+import {InstanceIdService} from "./instance-id.service";
 
 describe('AlcwebsocketService', () => {
   let service: AlcwebsocketService;
+  let instanceIdService: jasmine.SpyObj<InstanceIdService>;
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const instanceIdServiceStub =  jasmine.createSpyObj('InstanceIdService', ['getNextId']);
+    instanceIdServiceStub.getNextId.and.returnValue(0);
+    //ALC. [Объекты Spy ⚡️ Angular с примерами кода](https://angdev.ru/doc/unit-testing-spy-objects/)
+    //const appServiceSpy = spyOn(instanceIdService, 'getNextId'); - could be easily replaced by direct calls of similar functions on stub
+
+    TestBed.configureTestingModule({
+      providers:[{provide: InstanceIdService , useValue: instanceIdServiceStub}]
+    });
     service = TestBed.inject(AlcwebsocketService);
+    instanceIdService = TestBed.inject(InstanceIdService) as jasmine.SpyObj<InstanceIdService>;
   });
 
-  it('should be created', () => {
+  it('should be created with instance id = 0', () => {
     expect(service).toBeTruthy();
+    expect(service.getInstanceId()).toBe(0);
+    expect(instanceIdService.getNextId).toHaveBeenCalled();
+    expect(instanceIdService.getNextId.calls.count()).toBe(1)
   });
+
 });
